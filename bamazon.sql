c2881cbbc19cbbeaa6f6b4cca17a90bf62469ed8
Kimberlyn Creary <kimocreary@gmail.com>
2019-11-01 13:00:56 -0400

    functions added

--- bamazon.sql
+++ bamazon.sql
@@ -1,47 +1,29 @@
 DROP DATABASE IF EXISTS bamazon_db;
 CREATE DATABASE bamazon_db;
 USE bamazon_db;
+DROP TABLE products;
 
 CREATE TABLE products(
-  id INTEGER(11) AUTO_INCREMENT NOT NULL,
-  product_name VARCHAR(100),
-  department_name VARCHAR(50),
-  price INTEGER
-  stock_quantity INTEGER(1000),
+  id INT AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(100)NOT NULL,
+  department_name VARCHAR(50)NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  stock_quantity INT NOT NULL,
+  PRIMARY KEY(id)
 );
 
+SELECT * FROM products;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Bacon Socks', 'Mens Fashion', 5, 60);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('My Little Pony Headband', 'Accessories', 6, 125);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Black & Decker Iron', 'Household', 40, 37);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Everyday Craft Table', 'Furniture', 75, 8);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Samsung TV ', 'Electronics', 400, 15);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Cheeze-Its', 'Pantry', 2.99, 98);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Caress Soap', 'Personal Care', 1.99, 175);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Word-Search', 'Books', 7.99, 15);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Jawbone Bluetooth Speaker', 'Electronics', 80, 6);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('The Pioneer Woman Dinnerware Set', 'Housewares', 39.99, 14);
 
 
-INSERT INTO products (id, product_name, department_name, price, stock_quantity) values ('200', 'bacon socks', 'mens fashion', '5', 60);
-INSERT INTO products (id, product_name, department_name, price, stock_quantity) values ('201', 'headband', 'accessories', '6', 125);
-INSERT INTO products (id, product_name, department_name, price, stock_quantity) values ('202', 'iron', 'household', '40', 37);
-INSERT INTO products (id, product_name, department_name, price, stock_quantity) values ('203', 'craft table', 'furniture', '75', 8);
-INSERT INTO products (id, product_name, department_name, price, stock_quantity) values ('204', 'tv', 'electronics', '400', 15);
-INSERT INTO products (id, product_name, department_name, price, stock_quantity) values ('205', 'cheeze-its', 'pantry', '2.99', 98);
-INSERT INTO products (id, product_name, department_name, price, stock_quantity) values ('206', 'soap', 'personal care', '1.99', 175);
-INSERT INTO products (id, product_name, department_name, price, stock_quantity) values ('207', 'word-search', 'books', '7.99', 15);
-INSERT INTO products (id, product_name, department_name, price, stock_quantity) values ('208', 'bluetooth speaker', 'electronics', '80', 6);
-INSERT INTO products (id, product_name, department_name, price, stock_quantity) values ('209', 'dinnerware set', 'housewares', '39.99', 14);
 
-
-
-SELECT * FROM authors;
-SELECT * FROM books;
-
--- show ALL books with authors
--- INNER JOIN will only return all matching values from both tables
-SELECT title, firstName, lastName
-FROM books
-INNER JOIN authors ON books.authorId = authors.id;
-
--- show ALL books, even if we don't know the author
--- LEFT JOIN returns all of the values from the left table, and the matching ones from the right table
-SELECT title, firstName, lastName
-FROM books
-LEFT JOIN authors ON books.authorId = authors.id;
-
--- show ALL books, even if we don't know the author
--- RIGHT JOIN returns all of the values from the right table, and the matching ones from the left table
-SELECT title, firstName, lastName
-FROM books
-RIGHT JOIN authors ON books.authorId = authors.id;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,12 +1,73 @@
 var mysql = require("mysql");
+var inquire = require("inquirer");
+require("console.table");
 
 var connection = mysql.createConnection({
-    host: "localhost",
 
-    port: 4000,
-  
-    user: "root",
+  host: "localhost",
 
-    password: "Chiwale1",
-    database: "bamazon_db"
+  port: 3306,
+
+  user: "root",
+
+  password: "Chiwale1",
+  database: "bamazon_db"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  afterConnection();
+});
+
+function afterConnection() {
+  connection.query("SELECT * FROM products", function (err, data) {
+    if (err) throw err;
+    console.table(data);
   });
+}
+
+function getUserInput() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "choice",
+      message: "Enter ID of the product or (Q) to quit",
+      validate: function (val) {
+        return !isNaN(val) || val.toUpperCase() === "Q";
+      }
+
+
+    }
+  ]).then(function (val) {
+    var userId = val.choice;
+    if (userId === "Q") {
+      connection.end()
+      console.log("See You Next Time.  Bye")
+    }
+    else {
+      enterQuantity(userId)
+    }
+  })
+}
+
+function enterQuantity(userId) {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "quantity",
+      message: "Enter quantity of the product or (Q) to quit",
+      validate: function (val) {
+        return (val > 0) || val.toUpperCase() === "Q";
+      }
+
+
+    }
+  ]).then(function (val) {
+    var userQuantity = val.quantity
+    connection.query("SELECT * FROM products where id = ?",userId, function (err, data) {
+      if (err) throw err;
+      console.table(data);
+    });
+  })
+}
